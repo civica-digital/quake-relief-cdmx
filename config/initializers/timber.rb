36d0724c372f294607b5c40283494202c420208d
Mr. Outis <mroutis@protonmail.com>
2017-09-19 19:40:31 -0500

    Add timber for logging

---
+++ config/initializers/timber.rb
@@ -0,0 +1,21 @@
+# Timber.io Ruby Configuration - Simple Structured Logging
+#
+#  ^  ^  ^   ^      ___I_      ^  ^   ^  ^  ^   ^  ^
+# /|\/|\/|\ /|\    /\-_--\    /|\/|\ /|\/|\/|\ /|\/|\
+# /|\/|\/|\ /|\   /  \_-__\   /|\/|\ /|\/|\/|\ /|\/|\
+# /|\/|\/|\ /|\   |[]| [] |   /|\/|\ /|\/|\/|\ /|\/|\
+# -------------------------------------------------------------------
+# Website:       https://timber.io
+# Documentation: https://timber.io/docs
+# Support:       support@timber.io
+# -------------------------------------------------------------------
+
+config = Timber::Config.instance
+
+config.integrations.action_view.silence = Rails.env.production?
+
+# Add additional configuration here.
+# For a full list of configuration options and their explanations see:
+# http://www.rubydoc.info/github/timberio/timber-ruby/Timber/Config
+
+config.integrations.rack.http_events.collapse_into_single_event = Rails.env.production?
