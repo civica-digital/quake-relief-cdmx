4677acfc263fd45ef278723a90ae8d3ec282dd89
Rafael Cárdenas <rafael@rafaelcr.com>
2017-09-19 20:00:18 -0500

    Add worker to scan tweets (#2)

--- app/workers/twitter_worker.rb
+++ app/workers/twitter_worker.rb
@@ -2,6 +2,33 @@ class TwitterWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    
+    hashtags = %w(
+      #sismo
+      #CDMX
+      #SismoMexico
+      #FuerzaMexico
+      #prayformexico
+      #condesa
+      #roma
+      #laroma
+      #mexico
+      #TenemosSismo
+      #Narvarte
+      #niñosheroes
+      #sismocdmx
+      #delvalle
+      #delegacionbenitojuarez
+      #ayuda
+      #puebla
+      #terremoto
+      #temblor
+      sismomx
+      #SOSCDMX
+      #Sismomexico
+      #Ayudasismo
+    )
+    hashtags.each { |hashtag|
+      TwitterScanner.scan(hashtag)
+    }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920002024) do
+ActiveRecord::Schema.define(version: 20170920003857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170920002024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "url"
+    t.float    "lat"
+    t.float    "lng"
   end
 
 end
