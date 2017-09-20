cd074e159ff7f5ef94ff4a86628297c49fc303c2
Rafael Cardenas <rafael@rafaelcr.com>
2017-09-20 12:48:20 -0500

    Find tweets by neighborhood keywords

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
+require_relative '../../lib/neighborhoods'
+
 class Tweet < ApplicationRecord
+  scope :by_neighborhood, -> (neighborhood) { by_keywords(Neighborhoods.find(neighborhood)) }
+  scope :by_keywords, -> (keywords) {
+    out = []
+    keywords.each { |keyword|
+      out << Tweet.where("text LIKE ?", "%#{keyword}%")
+    }
+    return out
+  }
 end
