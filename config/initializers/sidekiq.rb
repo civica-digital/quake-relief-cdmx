70cdcaae25f815b1271022ab0d2a295d807538a5
Rafael Cárdenas <rafael@rafaelcr.com>
2017-09-19 20:06:06 -0500

    Add rake task to start scanning (#3)

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+require 'sidekiq'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379" } }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://redis:6379" } }
+end
