c3bbaad52e86d11b87f5d8853846fead1e4c8311
Rafael Cardenas <rafael@rafaelcr.com>
2017-09-21 11:31:51 -0500

    Get nearest location from API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   end
 
   resources :neighborhoods, only: [:show]
+  resources :needs, only: [] do
+    get :by_location, on: :collection
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'static_pages#index'
